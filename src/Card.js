19e7382da6f0c39c1f4aae14e6b972f1f17bdc31
rstallingsiii <59840429+rstallingsiii@users.noreply.github.com>
2024-08-24 23:25:32 -0500

    Finished first lesson on the robo friends app.

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Card = () => {
+
+const Card = (props) => {
     return (
-        <div>
-            <img src="" alt="" />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={'https://robohash.org/${props.id}?200x200'} alt="robots" />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
---
+++ src/ExampleButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExampleButton = () => {
+    return (
+        <button>
+            Press this button, or else.
+        </button>
+    );
+}
+
+export default ExampleButton;
